72b2d1aebc06bda10ff793c7a49aac7867813f7f
Deep Raj <deepraj6897@gmail.com>
2025-06-26 21:07:28 +0530

    added script for larger screen

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 let menu = document.querySelector('#menu');
 let navbar = document.querySelector('.navbar');
 
-//main header onclick toggle
+//main header onclick
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-    searchIcon.classList.remove('fa-times');
-    searchForm.classList.remove('active');
 }
 
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+
 let searchIcon = document.querySelector('#search-icon');
 let searchForm = document.querySelector('.search-form');
 
@@ -20,8 +15,6 @@ let searchForm = document.querySelector('.search-form');
 searchIcon.onclick = () => {
     searchIcon.classList.toggle('fa-times');
     searchForm.classList.toggle('active');
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
 }
 
 window.onscroll = () => {
